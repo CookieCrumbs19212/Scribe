3cca3e4244861e0a1fbbdc499a7ccf730b445be9
CookieCrumbs19212 <developer19212@gmail.com>
2022-09-04 00:16:41 +0200

    Added TODO

--- scribe.sh
+++ scribe.sh
@@ -6,3 +6,24 @@
 
 # set local backup location
 
+# initial set up walkthrough
+
+# command to add/remove files to backup list
+
+# command to add/remove files to exclude list
+
+# command to modify number of old backups.
+
+# command to setup new external backup device
+
+# command to turn on tar verbose logging
+
+# command to change local backup location
+
+# command to change external backup path
+
+# command to set filename-prefix
+
+# command to change backup type, external/local
+
+# 
