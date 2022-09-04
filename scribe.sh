3aff178f17b854ad65d664e430f374bd076b52f6
CookieCrumbs19212 <developer19212@gmail.com>
2022-09-04 23:22:36 +0200

    Added check for log directory and log files.
    Added checks for config directory and config files.
    
    Added switch case to analyze commands.

--- scribe.sh
+++ scribe.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 
 # Source the utils.sh script.
-. utils.sh
+. script/utils.sh
+
+# Source the setters.sh script.
+. scripts/setter.sh
+
+# Source the backup.sh script.
+. scripts/backup.sh
+
 
 # Paths to directories.
 CONFIG_DIR="config"
@@ -19,49 +26,10 @@ TAR_LOG="${LOG_DIR}/tar_verbose.log"
 TAR_ERROR_LOG="${LOG_DIR}/tar_errors.log"
 
 
-# If the config folder does not exist, create it and the files inside.
-if [[ ! -d $CONFIG_DIR ]]; then
-    # Making the config directory.
-    mkdir $CONFIG_DIR
-
-    # Create the config files inside the config directory.
-    touch $CONFIG_FILE $BACKUP_LIST $EXCLUDE_LIST
-fi
-
-# Creating an array of the files inside the config/ directory.
-CONFIG_FILES_ARRAY=($CONFIG_FILE $BACKUP_LIST $EXCLUDE_LIST)
-# Running a loop to create the config files if they do not exist.
-for file in "${CONFIG_FILES_ARRAY[@]}"
-do
-    # If the config file does not exist, create it.
-    if [[ ! -f $file ]]; then
-        # Create the config file.
-        touch $file
-    fi
-done
-
-
-# If the config file is empty, set defaults.
-if [[ ! -s $CONFIG_FILE ]]; then
-    # Setting to defaults.
-    reset_config_defaults
-
-    # Writing to the config file.
-    write_to_config_file
-fi
-
-
-# Source the config file.
-. $CONFIG_FILE
-
-
-# If the logs folder does not exist, create it and the files inside.
+# If the logs folder does not exist, create it.
 if [[ ! -d $LOG_DIR ]]; then
     # Making the logs directory.
     mkdir $LOG_DIR
-
-    # Create the log files inside the logs directory.
-    touch $MAIN_LOG $TAR_LOG $TAR_ERROR_LOG
 fi
 
 # Creating an array of the files inside the logs/ directory.
@@ -77,5 +45,92 @@ do
 done
 
 
+# Start logging from this point.
+log -b
 
 
+# If the config folder does not exist, create it.
+if [[ ! -d $CONFIG_DIR ]]; then
+    log -e "config/ directory not found"
+
+    # Making the config directory.
+    mkdir $CONFIG_DIR && log -d "Created config/ directory"
+fi
+
+# Creating an array of the files inside the config/ directory.
+CONFIG_FILES_ARRAY=($CONFIG_FILE $BACKUP_LIST $EXCLUDE_LIST)
+# Running a loop to create the config files if they do not exist.
+for file in "${CONFIG_FILES_ARRAY[@]}"
+do
+    # If the config file does not exist, create it.
+    if [[ ! -f $file ]]; then
+        log -e "${file} file is missing"
+
+        # Create the config file.
+        touch $file && log -d "Created ${file}"
+    fi
+done
+
+# If the config file is empty, set defaults.
+if [[ ! -s $CONFIG_FILE ]]; then
+    log -e "scribe.conf file is empty"
+
+    # Setting to defaults.
+    reset_config_defaults
+
+    log -d "Reset configurations to default"
+
+    # Writing to the config file.
+    write_to_config_file
+fi
+
+
+# Source the config file.
+. $CONFIG_FILE
+
+
+# Analyzing command.
+case "$1" in
+    --set-limit)
+        set_backup_limit $2
+    ;;
+
+    --set-prefix)
+        set_prefix $2
+    ;;
+
+    --set-backup-type | -t)
+        set_backup_type $2
+    ;;
+
+    --set-tar-verbose)
+        set_tar_verbose $2
+    ;;
+
+    --exclude-script)
+        exclude_script_files true
+    ;;
+
+    --include-script)
+        exclude_script_files false
+    ;;
+
+    --set-backup-loc)
+        set_backup_location $2
+    ;;
+
+    --set-signature)
+        set_signature_hash $2
+    ;;
+
+    --reset-config | --reset)
+        reset_config_defaults
+    ;;
+
+    backup)
+        create_backup
+    ;;
+
+    *)
+        echo "Invalid command"
+    ;;
