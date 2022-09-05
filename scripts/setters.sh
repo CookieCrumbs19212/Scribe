bd29946fa5c09c608facad5ffe5f43488d44d19e
CookieCrumbs19212 <developer19212@gmail.com>
2022-09-06 00:35:05 +0200

    Added checks for set_backup_limit.
    Added set_filename_prefix and illegal character checks for the same.
    
    Added filepath checks for set_backup_location.

--- scripts/setters.sh
+++ scripts/setters.sh
@@ -1,12 +1,38 @@
+# Setters for Scribe backup script.
+
+# Setter for the backup limit.
 function set_backup_limit {
-    BACKUP_LIMIT=$2
+    # Making sure that the input is a number.
+    if [[ ! "$1" =~ ^[0-9]+$ ]]; then
+        echo "input is not a number"
+        exit 1
+    else
+        # If the input is valid, assign it to BACKUP_LIMIT.
+        BACKUP_LIMIT=$1
+    fi
 }
 
-# Setting the backup location depending on BACKUP_TYPE.
-function set_backup_location {
-    if (( "$BACKUP_TYPE" == 1 )); then
-        BACKUP_LOC=$EXTERNAL_BACKUP_LOC
+
+# Setter for filename prefix.
+function set_filename_prefix {
+    # Check if the input contains any illegal filename characters.
+    # [[ "$1" == *\\* ]] || [[ "$1" =~ ['!@#$%^&*()+'] ]]
+    if [[ "$1" == */* ]]; then
+        echo "filename prefix contains illegal characters"
     else
+        # If the input does not contain illegal characters, assign to FILENAME_PREFIX.
+        FILENAME_PREFIX="$1"
+    fi
+}
+
+
+# Setter for the backup location.
+function set_backup_location {
+    # Check that the input points is a valid directory path.
+    if [[ ! -d "$1" ]]; then
+        echo "$1 is not a valid location or does not exist."
+    else
+        # If the input is valid, assign it to BACKUP_LOC.
         BACKUP_LOC=$LOCAL_BACKUP_LOC
     fi
 }
